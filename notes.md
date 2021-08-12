2342768cc364ebe14f5834b7062378a47608a17d
Michael Werneke <mike.werneke@gmail.com>
2021-08-12 18:15:06 -0500

    gallery items setup

---
+++ notes.md
@@ -0,0 +1,8 @@
+[x]Add a few images to pulic image folder
+[]modify server/modules/data to have an 'id' 'title' 'description' each id should be unique
+[]build an app.js, build gallery.js, and galleryItem.js
+
+[]Axios get from gallery and store in app.jsx
+[]gallery list pass it in the app via props loop over gallery data []make gllery items
+
+[]Update gallery each time a like button is clicked
--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'public/images/smores.png', description: 'Getting the Smores ready.', likes: 0 },
+    { id: 2, path: 'public/images/Funny.PNG', description: 'Clowning on Snapchat.', likes: 0 },
+    { id: 3, path: 'public/images/Hbaseball.png', description: 'Harrison, up to bat!', likes: 0 },
+    { id: 4, path: 'public/images/HannahLebanon.png', description: 'Out on our Hike at Lebanon.', likes: 0 },
+    { id: 5, path: 'public/images/Hannah.png', description: 'Hannah smiling.', likes: 0 },
+    { id: 6, path: 'public/images/pickleball.png', description: 'PPA event down in Georgia.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
