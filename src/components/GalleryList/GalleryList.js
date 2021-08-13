166538f5c721ad57e76a62379d84039434a29787
Michael Werneke <mike.werneke@gmail.com>
2021-08-12 22:41:45 -0500

    Button Icon is Clicking and recording

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import './GalleryList.css'
 import ThumbsUp from '@material-ui/icons/ThumbUp';
+import {useState} from 'react'
 
 function GalleryList({galleryList}) {
     
 //MUST DO BUTTONCLICK
-
+    let [count, setCount] = useState(0);
+    const onButtonClick = () =>{
+        setCount(count+1);
+        console.log('On Button Click',count);
+    
+    }
 
     
     return (
-        <div>
+       
+       <div>
             <div>
-                {galleryList.map((item)=>
+                {galleryList.map((item, i)=>
                     <div className= 'column'>
-                    <img className = 'image' src ={item.path}/>
-                    <button><ThumbsUp/></button>
+                    <img  className = 'image' src ={item.path}/>
+                    <button onClick={onButtonClick}><ThumbsUp/></button>
+                    <h4>Likes:{count}</h4>
                     </div>
+                    
+                    
+                    
                 )}
             </div>
         </div>
+        
     )
 }
 
