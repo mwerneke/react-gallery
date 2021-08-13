4e0538080960fc27f86a049a0dfea2729acc3985
Michael Werneke <mike.werneke@gmail.com>
2021-08-12 23:47:08 -0500

    Update to Gallery Items

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,1 +1,24 @@
+import {useState} from 'react'
+function GalleryItem({
+    picture
+}) {
+    
+    let [count, setCount] = useState(0);
+    const onButtonClick = () =>{
+        setCount(count+1);
+        console.log('On Button Click',count);
+    
+    }
 
+    return (
+       <div className='items'>
+           <img src ={picture.path}/>{picture.description}{picture.likes}
+            <h4>Likes:{count}</h4> 
+           <button onClick={onButtonClick}>Like</button>
+           </div>
+    )
+}
+
+export default GalleryItem;
+
+//<h4>Likes:{count}</h4>
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,30 +1,24 @@
 import React from 'react'
 import './GalleryList.css'
-import ThumbsUp from '@material-ui/icons/ThumbUp';
 import {useState} from 'react'
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 function GalleryList({galleryList}) {
     
-//MUST DO BUTTONCLICK
-    let [count, setCount] = useState(0);
-    const onButtonClick = () =>{
-        setCount(count+1);
-        console.log('On Button Click',count);
+
     
-    }
 
     
     return (
        
        <div>
             <div>
-                {galleryList.map((item, i)=>
+                {galleryList.map((picture)=>
                     <ul className= 'column'>
-                    <img src ={item.path}/>
-                    <div>
-                    <button onClick={onButtonClick}><ThumbsUp/></button>
-                    </div>
-                    <h4>Likes:{count}</h4>
+                    <GalleryItem 
+                        key={picture.id}
+                        picture={picture}
+                        />
                     </ul>
                     
                     
